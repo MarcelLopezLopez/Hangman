8fa27a7039152c150fe7e7facf4ca68cb2cc8b73
MarcelLopezLopez <marcelopez@estudiantat.upc.edu>
2023-12-11 10:51:16 +0100

    Juego iniciado

--- frontend/src/Juego.jsx
+++ frontend/src/Juego.jsx
@@ -26,48 +26,64 @@ const Juego = ({ creador, nombreUsuario, identificadorPartida }) => {
       // Actualizar el estado del juego en el cliente
       setLetra(mensaje);
     });
-
     // Limpia los listeners al desmontar el componente
     return () => {
-      socket.off('partidaIniciada');
+      socket.disconnect();
     };
   }, []);
 
-    // Enviar la letra adivinada al servidor
-    const handleEnviarLetra = async () => {
-      try {
-        const response = await fetch('http://localhost:8080/api/partida/letra', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ nombreUsuario: letra }),
-        });
-  
-        if (response.ok) {
-        } else {
-          console.error('Error al enviar la letra');
-        }
-      } catch (error) {
-        console.error('Error de red:', error);
+  // Enviar la letra adivinada al servidor
+  const handleEnviarLetra = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/partida/letra', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ nombreUsuario: letra }),
+      });
+
+      if (response.ok) {
+      } else {
+        console.error('Error al enviar la letra');
       }
-    };
+    } catch (error) {
+      console.error('Error de red:', error);
+    }
+  };
 
-    // Enviar la palabra cuando el usuario es el creador
-    const handleElegirPalabara = () => {
-      socket.emit('/app/submitWord', wordState);
-    };
-
-    return () => {
-      // Desconectar el socket cuando el componente se desmonta
-      socket.disconnect();
-    };
-  }, [guessedLetter, wordState, creador]);
+  // Enviar la palabra cuando el usuario es el creador
+  const handleElegirPalabara = () => {
+    socket.emit('/app/submitWord', palabraAdivinar);
+  };
 
   return (
     <div>
       <p>Nombre del Usuario: {nombreUsuario}</p>
       <p>Se encuentra ya en la Partida con id: {identificadorPartida}</p>
+      {creador ? (
+        <div>
+          <p>¡Eres el creador! Ingresa la palabra del juego:</p>
+          <input
+            type="text"
+            value={palabraAdivinar}
+            onChange={(e) => setPalabraAdivinar(e.target.value)}
+          />
+          <button onClick={handleElegirPalabara}>Enviar Palabra</button>
+        </div>
+      ) : (
+        <div>
+          <p>Número de letras en la palabra: {longitudPalabra}</p>
+          <p>Palabra actual: {palabraAdivinar}</p>
+          <input
+            type="text"
+            maxLength={1}
+            value={letra}
+            onChange={(e) => setLetra(e.target.value)}
+          />
+          <button onClick={handleEnviarLetra}>Adivinar</button>
+        </div>
+      )}
     </div>
   );
 };
